69899270feae2d026199a03f2e26ed16f9358bfb
conner-mcnicholas <connermcnicholas@gmail.com>
2022-07-27 20:20:12 -0700

    autocommit

--- FinalBuild/deploy_azure_resources/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-WORKSPACE=workspace
-docker run -it --rm \
-    -v $(pwd)/create_resources.sh:/${WORKSPACE}/create_resources.sh \
-    -v $(pwd)/.secrets:/${WORKSPACE}/.secrets \
-    -v $(pwd)/DATAFACTORY_pipelines:/${WORKSPACE}/DATAFACTORY_pipelines \
-    -w /${WORKSPACE} mcr.microsoft.com/azure-cli
